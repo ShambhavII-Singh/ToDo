bd4530bb2b0d1011a92bc3cac654bf04c98646d4
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-15 18:49:07 +0530

    add add-category-button 2

--- src/components/buttons/add-category-button.tsx
+++ src/components/buttons/add-category-button.tsx
@@ -12,3 +12,46 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+export const AddCategoryButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button variant="outline" className="flex justify-center items-center py-0 px-2 h-full rounded-l-none border-l-0">
+                <Plus /></Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>{`New Category`}</DialogTitle>
+                    <DialogDescription>
+                        {`Create a new task Category. Click save when you're done.`}
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                        Name
+                        </Label>
+                        <Input
+                        id="name"
+                        defaultValue=""
+                        className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="description" className="text-right">
+                        Description
+                        </Label>
+                        <Input
+                        id="username"
+                        defaultValue=""
+                        className="col-span-3"
+                        />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit">Save</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
