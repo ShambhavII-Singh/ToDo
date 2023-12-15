07a80e0d4975574519b9a418ecbc4ca7aa87845f
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-15 18:52:43 +0530

    add add-repository-link-button 2

--- src/components/buttons/repository-link-button.tsx
+++ src/components/buttons/repository-link-button.tsx
@@ -1,3 +1,11 @@
 import { Github } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
+export const RepositoryLinkButton = () => {
+
+    return (
+        <Button type="submit" variant="outline" size="icon" onClick={() => {window.location.href = 'https://github.com/ShambhavII-Singh/ToDo';}}>
+            <Github />
+        </Button>
+    )
+}
