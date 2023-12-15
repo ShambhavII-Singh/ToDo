44c78096f06df48a97d90045f8abdd7d62510a6d
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-15 18:48:49 +0530

    add add-category-button 1

---
+++ src/components/buttons/add-category-button.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
