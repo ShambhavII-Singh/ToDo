517fe0a6cd83cad2267c189506527b8617a4aa37
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-15 18:50:20 +0530

    add add-task-button 2

--- src/components/buttons/add-task-button.tsx
+++ src/components/buttons/add-task-button.tsx
@@ -12,4 +12,45 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-
+export const AddTaskButton = () => {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <PlusSquare className="bg-ring text-primary-foreground p-1 rounded" />  
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>{`New Task`}</DialogTitle>
+                    <DialogDescription>
+                        {`Create a new task. Click save when you're done.`}
+                    </DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="name" className="text-right">
+                        Name
+                        </Label>
+                        <Input
+                        id="name"
+                        defaultValue=""
+                        className="col-span-3"
+                        />
+                    </div>
+                    <div className="grid grid-cols-4 items-center gap-4">
+                        <Label htmlFor="description" className="text-right">
+                        Description
+                        </Label>
+                        <Input
+                        id="username"
+                        defaultValue=""
+                        className="col-span-3"
+                        />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit">Save</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
+}
