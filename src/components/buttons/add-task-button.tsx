5ecd93ecb40a103afde2ac9baba75978c942f53a
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2023-12-20 18:39:03 +0530

    add form to dialog 2

--- src/components/buttons/add-task-button.tsx
+++ src/components/buttons/add-task-button.tsx
@@ -1,16 +1,13 @@
 import { PlusSquare } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import {
     Dialog,
     DialogContent,
     DialogDescription,
-    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { AddTaskForm } from "../forms/add-task-form";
 
 export const AddTaskButton = () => {
     return (
@@ -25,31 +22,9 @@ export const AddTaskButton = () => {
                         {`Create a new task. Click save when you're done.`}
                     </DialogDescription>
                 </DialogHeader>
-                <div className="grid gap-4 py-4">
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="name" className="text-right">
-                        Name
-                        </Label>
-                        <Input
-                        id="name"
-                        defaultValue=""
-                        className="col-span-3"
-                        />
-                    </div>
-                    <div className="grid grid-cols-4 items-center gap-4">
-                        <Label htmlFor="description" className="text-right">
-                        Description
-                        </Label>
-                        <Input
-                        id="username"
-                        defaultValue=""
-                        className="col-span-3"
-                        />
-                    </div>
+                <div className="grid gap-4 pb-4">
+                    <AddTaskForm />
                 </div>
-                <DialogFooter>
-                    <Button type="submit">Save</Button>
-                </DialogFooter>
             </DialogContent>
         </Dialog>
     )
