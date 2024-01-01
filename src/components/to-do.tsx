ae5c0e704eef81fed6f6f1422b8b6efcfd22fb4e
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2024-01-01 16:37:11 +0530

    remove placeholders

--- src/components/to-do.tsx
+++ src/components/to-do.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import {
     Card,
     CardContent,
@@ -7,8 +6,6 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import {
     Tabs,
     TabsContent,
@@ -37,17 +34,10 @@ export const ToDo = () => {
                         </CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" defaultValue="Pedro Duarte" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="username">Username</Label>
-                            <Input id="username" defaultValue="@peduarte" />
-                        </div>
+                        
                     </CardContent>
                     <CardFooter>
-                        <Button>Save changes</Button>
+                        
                     </CardFooter>
                 </Card>
             </TabsContent>
@@ -60,17 +50,10 @@ export const ToDo = () => {
                         </CardDescription>
                     </CardHeader>
                     <CardContent className="space-y-2">
-                        <div className="space-y-1">
-                            <Label htmlFor="current">Current password</Label>
-                            <Input id="current" type="password" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="new">New password</Label>
-                            <Input id="new" type="password" />
-                        </div>
+                        
                     </CardContent>
                     <CardFooter>
-                        <Button>Save password</Button>
+                        
                     </CardFooter>
                 </Card>
             </TabsContent>
