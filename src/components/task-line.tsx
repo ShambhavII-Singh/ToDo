96d552469f047fb1cc58ea545d59f1e6e8c39e18
ShambhavII-Singh <shambhavi.singh2021@vitstudent.ac.in>
2024-01-01 16:36:00 +0530

    set up badge functionality

--- src/components/task-line.tsx
+++ src/components/task-line.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Checkbox } from "@/components/ui/checkbox";
+import { StarsIcon } from "lucide-react";
 
 type TaskDetails = {
     task: string,
@@ -11,10 +12,14 @@ export const  TaskLine = ({task,description} : TaskDetails) => {
     return (
         <div className="items-top flex space-x-2 w-full border-b-2 border-b-ring p-2 group relative ">
             <Checkbox checked={checked} onCheckedChange={() => {setChecked(!checked)}}/>
-            <div className="grid leading-none w-full">
-                <div className="flex justify-between items-center w-full">
-                    <label className={`text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${checked?' text-muted-foreground line-through':''}`}>
-                        {task}</label>
+            <div className="grid leading-none w-full flex-col items-center">
+                <div className="flex justify-between items-start w-full">
+                    <div className="flex justify-start gap-x-3">
+                        <label className={`text-xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${checked?' text-muted-foreground line-through':''}`}>
+                            {task}
+                        </label>
+                        <StarsIcon className="x-1 text-ring" />
+                    </div>
                     <p className="invisible group-hover:visible text-sm text-muted-foreground">Right click for more</p>
                 </div>
                 <p className="text-sm text-muted-foreground break-all">
